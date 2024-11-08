5316bc6910eee8028dae07470f13fcefec30f236
Alessioryan <alessio.ryan@gmail.com>
2024-11-08 09:35:02 -0500

    Upload save completed grids file

--- grid.py
+++ grid.py
@@ -2,6 +2,7 @@ import copy
 from collections import defaultdict
 import os
 import numpy as np
+from tqdm import tqdm
 
 
 def _make_byte_array_readable(byte_array):
@@ -25,6 +26,14 @@ def _make_grid_readable(grid):
     return '\n'.join(readable_rows)
 
 
+# A helper function to see if two words match
+def match(straight, word_to_check):
+    for straight_char, word_to_check_char in zip(straight, word_to_check):
+        if straight_char and straight_char != word_to_check_char:
+            return False
+    return True
+
+
 class Grid:
     # CAN ONLY HANDLE SQUARE GRIDS FOR NOW
     def __init__(self, main_dict_fp, grid_size=5):
@@ -40,7 +49,10 @@ class Grid:
         self.entry_order = [f"{i}-{letter}" for i in range(self.grid_size) for letter in ['a', 'd']]
         self.entry_filled = [False for _ in self.entry_order]
         self.entry_index = 0
-        self.completed_grids = set()
+        self.completed_grids = []
+
+        # Set verbose commands
+        self.verbose = defaultdict(bool)
 
     def __str__(self):
         return str(_make_grid_readable(self.grid))
@@ -87,18 +99,19 @@ class Grid:
 
     def get_possible_words(self, straight):
         words_to_check = self.dict[len(straight)]
+        if self.verbose['Print completed words']:
+            print(f"Checking {len(words_to_check)} words")
+        valid_words = [word_to_check for word_to_check in words_to_check if match(straight, word_to_check)]
+        if self.verbose['Print completed words']:
+            print(f"Found {len(valid_words)} words compatible with {straight} \n")
+        # TODO continue from here, this method needs to be made faster
+        return valid_words
 
-        # A helper function to see if two words match
-        def match(straight, word_to_check):
-            for straight_char, word_to_check_char in zip(straight, word_to_check):
-                if straight_char and straight_char != word_to_check_char:
-                    return False
-            return True
-
-        return [word_to_check for word_to_check in words_to_check if match(straight, word_to_check)]
-
-    # Sets the straight mentioned to the possible_word
-    def set_straight(self, possible_word, i_dir, i_dir_action):
+    # Sets the straight but doesn't do any updates
+    def _set_straight(self, possible_word, i_dir, check_possible=False):
+        if check_possible and not match(self.get_straight(i_dir=i_dir), word_to_check=possible_word):
+            raise Exception(f"Straight {possible_word} given i_dir {i_dir} does not fit in the following grid: \n"
+                            f"{_make_grid_readable(self.grid)}")
         index, direction = i_dir.split("-")
         index = int(index)
         if direction == "a":
@@ -107,6 +120,10 @@ class Grid:
             self.grid[:, index] = possible_word
         else:
             raise Exception(f"Invalid i_dir: {i_dir}")
+
+    # Sets the straight mentioned to the possible_word
+    def set_straight(self, possible_word, i_dir, i_dir_action):
+        self._set_straight(possible_word=possible_word, i_dir=i_dir, check_possible=False)
         # Update the i_dir according to the action
         if i_dir_action == "update":
             self.entry_filled[self.entry_index] = True
@@ -127,10 +144,13 @@ class Grid:
             raise Exception(f"Invalid i_dir_action: {i_dir_action}")
 
     # Recursive function that fills the grid
-    def fill_grid(self):
+    def fill_grid(self, max_completed_grids=100):
         # Successful exit condition
+        if len(self.completed_grids) == max_completed_grids:
+            return
         if self.entry_index == len(self.entry_order):
-            self.completed_grids.add(copy.deepcopy(self))
+            # print(f"Found completed grid: \n{_make_grid_readable(self.grid)} ")
+            self.completed_grids.append(self.grid.copy() )
             return
         # Get the current index and direction for the unsuccessful exit condition
         curr_i_dir = self.get_curr_i_dir()
@@ -140,19 +160,58 @@ class Grid:
         if not len(possible_words):
             return
         # DFS
-        for possible_word in possible_words:
+        words_iterator = tqdm(possible_words) if self.entry_index == 0 else possible_words
+        for possible_word in words_iterator:
             self.set_straight(possible_word, curr_i_dir, i_dir_action="update")
             self.fill_grid()
             self.set_straight(curr_straight, curr_i_dir, i_dir_action="revert")
 
+    # Manually fill out straights in the grid
+    # straights must be a list of tuples, consisting of direction and word
+    def manually_set_straights(self, straights):
+        for word, i_dir in straights:
+            word_array = np.array(list(word), dtype="S1")
+            self._set_straight(possible_word=word_array, i_dir=i_dir, check_possible=True)
+            # Now that we manually set the straights, we don't want them to be able to be removed
+            i_dir_index = self.entry_order.index(i_dir)
+            if i_dir_index == -1:
+                raise Exception(f"Illegal i_dir {i_dir} for {word}")
+            del self.entry_order[i_dir_index]
+            del self.entry_filled[i_dir_index]
+
+    # Helper method to get the output for printing grids and saving grids
+    def _get_completed_grids_output(self):
+        output = [f"====================There are {len(self.completed_grids)} grids====================\n"]
+        for completed_grid in self.completed_grids:
+            output.append(_make_grid_readable(completed_grid) + "\n")
+        return output
+
     # Print out the completed grids
     def print_completed_grids(self):
-        print(f"====================There are {len(self.completed_grids)} grids====================\n")
-        for completed_grid in self.completed_grids:
-            print(str(completed_grid) + "\n")
+        print("".join(self._get_completed_grids_output() ) )
+
+    # Saves the completed grids
+    # file_name can be a string or a tuple consisting of the straights and the dict name
+    # They will always save in the finished_crosswords directory
+    def save_completed_grids(self, file_name, print_grids=False):
+        output = self._get_completed_grids_output()
+        if print_grids:
+            print("".join(output) )
+        if isinstance(file_name, tuple):
+            file_straights, file_dict_name = file_name
+            file_straights = "__".join(f"{file_word}_{file_i_dir}" for file_word, file_i_dir in file_straights)
+            file_name = f"{file_straights}__{file_dict_name}"
+        elif not isinstance(file_name, str):
+            raise Exception("file_name must be a string or a tuple of straights and dict_name")
+        with open(os.path.join("finished_crosswords", file_name), "w") as output_file:
+            output_file.writelines(output)
 
 
 if __name__ == "__main__":
-    test_grid = Grid(main_dict_fp=os.path.join("dictionaries", "small_5_dict.txt"), grid_size=5)
+    straights = [("blunt", "2-a"), ("allay", "1-d")]
+    dict_name = "small_5_dict.txt"
+    test_grid = Grid(main_dict_fp=os.path.join("dictionaries", dict_name), grid_size=5)
+    test_grid.verbose['Print compatible words'] = True
+    test_grid.manually_set_straights(straights=straights)
     test_grid.fill_grid()
-    test_grid.print_completed_grids()
+    test_grid.save_completed_grids(file_name=(straights, dict_name), print_grids=True)
